5852893585e015e7d3e1c4347431adab5c4ae822
ferdoznik <ferdoznik@gmail.com>
2025-09-21 20:35:31 +0200

    HW5

---
+++ src/main/kotlin/lessons/lesson5/ElvisOperator.kt
@@ -0,0 +1,24 @@
+package lessons.lesson5
+
+class ElvisOperator {
+
+    fun main() {
+        // Задача 1
+        val initialIntensity: Double = 120.0
+        val attenuationCoeff: Double? = null
+        val effectiveCoeff = attenuationCoeff ?: 0.5
+        val attenuatedIntensity = initialIntensity * effectiveCoeff
+
+        // Задача 2
+        val deliveryCost: Double = 18.99
+        val cargoValue: Double? = null
+        val effectiveCargoValue = cargoValue ?: 50.0
+        val insurance = effectiveCargoValue * 0.005
+        val totalDeliveryCost = deliveryCost + insurance
+
+        // Задача 3
+        val pressure: String? = null
+        val attentionMessage = "Error! Pressure is low"
+        val pressureForLab = pressure ?: attentionMessage
+    }
+}
